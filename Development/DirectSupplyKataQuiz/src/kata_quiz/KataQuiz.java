70a86ad343fe450f82b3c07f5a3b8609466ccecd
Christine Weld <clweld@ncsu.edu>
2024-10-24 22:38:23 -0400

    [Test] git commit 2

--- Development/DirectSupplyKataQuiz/src/kata_quiz/KataQuiz.java
+++ Development/DirectSupplyKataQuiz/src/kata_quiz/KataQuiz.java
@@ -7,9 +7,10 @@ package kata_quiz;
  * 
  */
 public class KataQuiz {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Testing 123!");
 	}
 
 }
